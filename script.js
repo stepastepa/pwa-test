8a76472aad1f40ef8b3539e40b9d726adf7104cf
stepastepa <stepanisworking@gmail.com>
2025-07-07 12:17:03 +0300

    test: add manual focus for inputs

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('username').addEventListener('touchend', (e) => {
+  e.target.focus();
+});
+
+document.getElementById('password').addEventListener('touchend', (e) => {
+  e.target.focus();
+});
